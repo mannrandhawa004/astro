9dec0db19ebd6f582e5771fee9039527cac64b16
Manpreet Singh <mannrandhawa004@gmail.com>
2026-01-17 11:21:35 +0530

    deployement

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,8 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173", 
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://astro-avz7.onrender.com"
 ];
 
 
